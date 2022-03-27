05a6dfb09adbab2df023deb9316ab0b17b916b20
Travis Fischer <fisch0920@gmail.com>
2022-03-27 06:50:26 -0400

    feat: optimize robots.txt and sitemap.xml

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -3,22 +3,11 @@ import { SiteMap } from 'lib/types'
 import { host } from 'lib/config'
 import { getSiteMaps } from 'lib/get-site-maps'
 
-<<<<<<<< HEAD:pages/api/sitemap.xml.ts
-import { SiteMap } from '../../lib/types'
-import { host } from '../../lib/config'
-import { getSiteMaps } from '../../lib/get-site-maps'
-
-export default async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
-========
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
->>>>>>>> d1e495e (feat: Change sitemap.xml, robots.xml end-point):pages/sitemap.xml.tsx
   if (req.method !== 'GET') {
     res.statusCode = 405
-    res.setHeader("Content-Type", "application/json")
-    res.write(JSON.stringify({ error: "method not allowed" }))
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify({ error: 'method not allowed' }))
     res.end()
     return {
       props: {}
@@ -27,7 +16,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
   const siteMaps = await getSiteMaps()
 
-  // cache sitemap for up to one hour
+  // cache for up to one hour
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=3600, max-age=3600, stale-while-revalidate=3600'
@@ -42,34 +31,26 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 }
 
 const createSitemap = (siteMap: SiteMap) =>
-  `
-  <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>${host}</loc>
-      </url>
+  `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${host}</loc>
+    </url>
 
-      <url>
-        <loc>${host}/</loc>
-      </url>
+    <url>
+      <loc>${host}/</loc>
+    </url>
 
-      ${Object.keys(siteMap.canonicalPageMap)
-        .map((canonicalPagePath) =>
-          `
-            <url>
-              <loc>${host}/${canonicalPagePath}</loc>
-            </url>
-          `.trim()
-        )
-        .join('')}
-    </urlset>
-<<<<<<<< HEAD:pages/api/sitemap.xml.ts
-  </xml>
-  `
-========
-    `
+    ${Object.keys(siteMap.canonicalPageMap)
+      .map((canonicalPagePath) =>
+        `
+          <url>
+            <loc>${host}/${canonicalPagePath}</loc>
+          </url>
+        `.trim()
+      )
+      .join('')}
+  </urlset>
+`
 
-const SiteMapXml: React.FC = () => null
-
-export default SiteMapXml
->>>>>>>> d1e495e (feat: Change sitemap.xml, robots.xml end-point):pages/sitemap.xml.tsx
+export default () => null
