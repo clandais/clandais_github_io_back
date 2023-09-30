2fe082e5ca84f9b956060f45a9499de9b956049b
Clément L. <clementlandais@gmail.com>
2023-09-30 21:17:51 +0200

    "images.unoptimized: true"

--- next.config.js
+++ next.config.js
@@ -33,6 +33,6 @@ module.exports = withBundleAnalyzer({
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    optimizeImages: false,
+    unoptimized: true
   }
 })
