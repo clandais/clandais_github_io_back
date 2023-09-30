e0fcd4d73a863b018e33d00cd2fc95c9126275b1
Clément L <clementlandais@gmail.com>
2023-09-30 21:02:54 +0200

    Test deplyment to gh-pages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,22 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = withBundleAnalyzer({
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
