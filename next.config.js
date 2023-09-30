f4c218f99c0b963a15c5e2bad0f2f02f354464b6
Clément L. <clementlandais@gmail.com>
2023-09-30 21:14:28 +0200

    Trying with "optimizeImages" to "false"

--- next.config.js
+++ next.config.js
@@ -32,6 +32,7 @@ module.exports = withBundleAnalyzer({
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    optimizeImages: false,
   }
 })
