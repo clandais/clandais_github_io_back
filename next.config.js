136224bdd159e9ef7a9620bd0b23d1b5615e309c
Clément L. <clementlandais@gmail.com>
2023-09-30 21:31:00 +0200

    Rollback to use vercel ?

--- next.config.js
+++ next.config.js
@@ -3,22 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 module.exports = withBundleAnalyzer({
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
@@ -32,7 +17,6 @@ module.exports = withBundleAnalyzer({
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: true
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
 })
